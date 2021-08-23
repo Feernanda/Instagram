88e5cbcc997274a96ebc2f1e6ef88801145740cd
unknown <lidia.suarez@polimentes.com>
2021-08-23 16:31:25 -0500

    Created the project structure and Footer HTML/CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { ExploreComponent } from './components/page/explore/explore.component';
+import { ConfigurationComponent } from './components/page/configuration/configuration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'configuration', component: ConfigurationComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { ExploreComponent } from './components/page/explore/explore.component';
+import { ConfigurationComponent } from './components/page/configuration/configuration.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { MainComponent } from './components/layout/main/main.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FeedComponent } from './components/layout/main/feed/feed.component';
+import { StoriesComponent } from './components/layout/main/stories/stories.component';
+import { SidebarComponent } from './components/layout/main/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExploreComponent,
+    ConfigurationComponent,
+    FooterComponent,
+    MainComponent,
+    NavbarComponent,
+    FeedComponent,
+    StoriesComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
